27553261102dfadf721c37efcfd98adc4ef36c3e
David Monarrez <davidmonarrez3@gmail.com>
2019-01-25 12:03:43 -0800

    got the logic working. i was making it to complicated

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,40 @@
 //check if first digit is 1 2 or 3
 //if it isnt check the next diget (if possible) or print number
 function checkFor321(num) {
-  if(num == 3) {
-    return "I'm sorry, Dave. I'm afraid I can't do that."
-  } else if(num == 2) {
-    return 'Boop!'
-  } else if(num == 1) {
-    return 'Beep!'
+  let value
+  if(num.includes('3')){
+    value = "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if(num.includes('2')){
+    value = 'boop';
+  } else if(num.includes('1')){
+    value = 'beep';
   } else {
-    return num
+    value = num;
   }
+  return value
 }
+//checkFor321('32');
 
 function numbersLessThan(num) {
   let numsArr = [];
-  for(var i = 0; i < num; i ++){
+  for(var i = 0; i <= num; i ++){
     let numString = i.toString();
     numsArr.push(numString);
   }
+  //returns an array of numbers type:string
   return numsArr
 }
 
+
 function beepBoop(num) {
+  //debugger;
   let beepBoopArr = [];
   let numsLessThan = numbersLessThan(num);
-  //for every num less than num
-  for(let i = 0; i < numsLessThan.length; i++) {
-    //for every digit in numLessThan[i]
-    for(let j = 0; j < numsLessThan[i].length; j++) {
-        beepBoopArr.push(checkFor321(numbersLessThan[i][j]));
-    }
+
+  for(var i = 0; i < numsLessThan.length; i++) {
+    beepBoopArr.push(checkFor321(numsLessThan[i]));
   }
+  return beepBoopArr
 }
 
 
@@ -41,6 +45,6 @@ $(document).ready(function (){
     //input sentence
     let number = $("#number").val();
 
-    $("#result").text();
+    $("#result").text(beepBoop(number));
   });
 });
